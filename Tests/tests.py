1d25a8e4259161575d0f8cac68e470a3f8745d99
Joao MIguel <jmocarvallho7123@gmail.com>
2025-10-04 16:58:50 -0300

    Learn Python OOP

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,36 +1,44 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
-    def eat(self):
-        print(f"{self.name} is eating")
 
-    def sleep(self):
-        print(f"{self.name} is sleeping")
+class Shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
 
-class Prey(Animal):
-    def flee(self):
-        print(f"{self.name} is fleeing")
+    def describe(self):
+        print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'}")
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+    
+    def describe(self):
+        super().describe()
+        print(f"It is a circle with an area of {self.radius**2*3.14}cm²")
+
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+    def describe(self):
+        super().describe()
+        print(f"It is a square with an area of {self.width**2}cm²")
+
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+    def describe(self):
+        super().describe()
+        print(f"It is a triangle with an area of {self.width * self.height / 2}cm²")
+
+circle = Circle("Red", True, 5)
+square = Square("Blue", False, 6)
+triangle = Triangle("Yellow", True, 7, 8)
 
 
-class Predator(Animal):
-    def hunt(self):
-        print(f"{self.name} is hunting")
+triangle.describe()
 
-
-class Rabbit (Prey):
-    pass
-
-
-class Hawk (Predator):
-    pass
-
-
-class Fish (Prey, Predator):
-    pass
-
-
-rabbit = Rabbit("Bugs")
-hawk = Hawk("Tony")
-fish = Fish("Nemo")
-
-fish.hunt()
